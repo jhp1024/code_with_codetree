bc63fdc63378601919ad0bc5144857238b8ff250
jhp1024 <87648963+jhp1024@users.noreply.github.com>
2025-08-02 18:59:21 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 165ms, Memory: 26MB, Status: Wrong Answer - Codetree

---
+++ 250802/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[][] checked = new int[2000][2000];
+    public static int offset = 1000;
+
+    public static void check(int x1, int y1, int x2, int y2, boolean isFirst) {
+        for (int i = x1; i < x2; i++) {
+            for (int j = y1; j < y2; j++){
+                if (isFirst) {
+                    checked[i][j] = 1;
+                } else {
+                    checked[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int result = 0;
+        int min_x = 2000;
+        int min_y = 2000;
+        int max_x = 0;
+        int max_y = 0;
+        Scanner sc = new Scanner(System.in);
+        int rect1_x1 = offset + sc.nextInt();
+        int rect1_y1 = offset + sc.nextInt();
+        int rect1_x2 = offset + sc.nextInt();
+        int rect1_y2 = offset + sc.nextInt();
+        int rect2_x1 = offset + sc.nextInt();
+        int rect2_y1 = offset + sc.nextInt();
+        int rect2_x2 = offset + sc.nextInt();
+        int rect2_y2 = offset + sc.nextInt();
+        // Please write your code here.
+        check(rect1_x1, rect1_y1, rect1_x2, rect1_y2, true);
+        check(rect2_x1, rect2_y1, rect2_x2, rect2_y2, false);
+        for (int i = 0; i < 2000; i++) {
+            for (int j = 0 ; j < 2000; j++){
+                if (checked[i][j] == 1) {
+                    if (min_x > i || min_y > j) {
+                        min_x = i;
+                        min_y = j;
+                    }
+
+                    if (max_x < i || max_y < j) {
+                        max_x = i+1;
+                        max_y = j+1;
+                    }
+                }
+            }
+        }
+        System.out.println((max_y-min_y)*(max_x-min_x));
+    }
+}
